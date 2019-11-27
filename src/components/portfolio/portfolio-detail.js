4d4d90f125eb163aef975653850da7b2797e0510
Felicia Wilkes <feliciankc@gmail.com>
2019-11-26 21:48:58 -0600

    Initial Build Out of the Portfolio Detail Component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default function(props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug} </h2>
-    </div>
-  );
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://feliciawilkes.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(error => {
+        console.log("getportfolioitem errors", error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug} </h2>
+      </div>
+    );
+  }
 }
