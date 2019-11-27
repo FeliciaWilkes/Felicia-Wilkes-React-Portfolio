9a294354f1a8fb6379fa22242e443774b76b3081
Felicia Wilkes <feliciankc@gmail.com>
2019-11-26 21:07:46 -0600

    Building Contact Page Styles for the React Portfolio

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -3,12 +3,24 @@ import {
   faSignOutAlt,
   faEdit,
   faSpinner,
-  faPlusSquare
+  faPlusSquare,
+  faPhone,
+  faEnvelope,
+  faMapMarkedAlt
 } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons = () => {
-  return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusSquare);
+  return library.add(
+    faTrash,
+    faSignOutAlt,
+    faEdit,
+    faSpinner,
+    faPlusSquare,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt
+  );
 };
 
 export default Icons;
